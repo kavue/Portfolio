90c2a2bf9850a24923deb19515ba6dc54492fceb
kavue <kvue06@gmail.com>
2024-12-04 21:12:12 -0600

    Added code to Portfolio, About Me, and App.css files

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -5,7 +5,7 @@ const AboutMe = () => (
   <section className="about-me">
     <h1>About Me</h1>
     <img src={myImage} alt="Developer" className="avatar" />
-    <p>Hello! I'm a new beginner coder with a passion for learning and growing in the tech world. I recently embarked on my coding 
+    <p>Hello! I'm a beginner programmer with a passion for learning and growing in the tech world. I recently embarked on my coding 
       journey and am excited to expand my skills in web development, programming languages, and creating meaningful applications. I enjoy 
       solving problems and embracing challenges, and I am always eager to learn new technologies and techniques to enhance my coding knowledge.</p>
   </section>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,20 +1,34 @@
+import AutoREADME from '../assets/AutoREADME.jpg';
+import EmployeeTracker from '../assets/EmployeeTracker.jpg';
+import VehicleBuilder from '../assets/VehicleBuilder.jpg';
+import WeatherDashboard from '../assets/WeatherDashboard.jpg';
+import CosmicQuest from '../assets/CosmicQuest.png';
+import { FaGithub } from 'react-icons/fa';
+
 const Portfolio = () => {
     const projects = [
-      { title: 'Project 1', image: 'image1.jpg', live: '#', repo: '#' },
-      // Add more projects
+      { title: 'Auto README', image: AutoREADME, live: '#', repo: '#' },
+      { title: 'Employee Tracker', image: EmployeeTracker, live: '#', repo: '#' },
+      { title: 'Vehicle Builder', image: VehicleBuilder, live: '#', repo: '#' },
+      { title: 'Weather Dashboard', image: WeatherDashboard, live: '#', repo: '#' },
+      { title: 'Cosmic Quest', image: CosmicQuest, live: '#', repo: '#' },
     ];
   
     return (
       <section className="portfolio">
         {projects.map((project, index) => (
           <div key={index} className="portfolio-item">
-            <img src={project.image} alt={project.title} />
-            <h3>{project.title}</h3>
+            <div className="image-container">
+              <img src={project.image} alt={project.title} />
+              <div className="image-overlay">
+                <h3>{project.title}</h3>
+                <a href={project.repo} target="_blank" rel="noopener noreferrer" className="github-icon">
+                  <FaGithub size={30} />
+                </a>
+              </div>
+            </div>
             <a href={project.live} target="_blank" rel="noopener noreferrer">
               Live App
-            </a>
-            <a href={project.repo} target="_blank" rel="noopener noreferrer">
-              GitHub Repo
             </a>
           </div>
         ))}
